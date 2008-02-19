e4f8828e168a1bf31b79c510192d5b0157eefa55
michele.bologna <michele.bologna@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-02-19 17:16:02 +0000

    xmltvgrabber modified. added lib/ added jdom to lib

--- jTVGuide/src/xmltv/XMLTVGrabber.java
+++ jTVGuide/src/xmltv/XMLTVGrabber.java
@@ -1,22 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class XMLTVGrabber implements XMLTVHelper {
 
-    private XMLTVParser mXMLTVParser;
+	private XMLTVParser mXMLTVParser;
 
-    public XMLTVGrabber () {
-    }
+	public XMLTVGrabber () {
+	}
 
-    public boolean grabSchedule (XMLTVHelper options) {
-        return true;
-    }
+	public boolean grabSchedule (XMLTVHelper options) {
+		/* devo richiamare il grabber di xmltv */
+		String OS = System.getProperty("os.name").toLowerCase();
+		if (OS.indexOf("windows") > -1) {
+			/* siamo in Windows */
+			String command = "xmltv.exe " + mXMLTVGrabbers.getGrabber(mUserPreferences.getLocation());
+		}
+		else {
+			/* UNIX e derivati */
+			String command = mXMLTVGrabbers.getGrabber(mUserPreferences.getLocation());
+		}
 
-    public XMLTVParser getXMLTVParser () {
-        return mXMLTVParser;
-    }
+		String parameters = userPreferences.getOptions();
+		
+		Process p = Runtime.getRuntime().exec(command + " " + parameters);
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        p.waitFor();
+        status = p.exitValue();
+		return (status == 0) ? true : false;
+		mXMLTVParser.parse(xmltvOutput);
+	}
 
-    public void setXMLTVParser (XMLTVParser val) {
-        this.mXMLTVParser = val;
-    }
+	public XMLTVParser getXMLTVParser () {
+		return mXMLTVParser;
+	}
+
+	public void setXMLTVParser (XMLTVParser val) {
+		this.mXMLTVParser = val;
+	}
 
 }
 
