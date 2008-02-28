a0f7d8957470875a5a7316911cbe542f313fd032
sebastiano.rota@tiscali.it <sebastiano.rota@tiscali.it@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-02-28 15:32:30 +0000

    channelVector from mUserPreferences.getXmltvConfigFile()

--- jTVGuide/src/xmltv/XMLTVConfigurator.java
+++ jTVGuide/src/xmltv/XMLTVConfigurator.java
@@ -1,3 +1,7 @@
+package xmltv;
+
+import java.io.*;
+import java.util.Vector;
 
 public class XMLTVConfigurator implements XMLTVHelper {
 
@@ -5,7 +9,30 @@ public class XMLTVConfigurator implements XMLTVHelper {
     }
 
     public void config () {
+
+    	File xmltvConfig = mUserPreferences.getXmltvConfigFile();
+    	Vector<String> channelVector = new Vector<String>();
+    	Vector<Boolean> channelSelected = new Vector<Boolean>();
+    	try {
+			BufferedReader reader = new BufferedReader(new FileReader(xmltvConfig));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				channelVector.add(line.substring(line.lastIndexOf("#")+2));
+				channelSelected.add(false);
+				/* in channelVector ho la lista dei canali,
+				 * channelSelected ha le stesse dimensioni di channelVector,
+				 * di default nessun canale è selezionato dal cconfiguratore
+				 */
+	        }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
     }
+
+
 
 }
 
