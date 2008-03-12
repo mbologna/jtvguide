9357960fc40928caf5a568be14fee0cdec9de721
michele.bologna <michele.bologna@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-03-12 12:51:14 +0000

    - Parsing channel and shows - Channel structure

--- jTVGuide/src/jtvguide/jTVGuide.java
+++ jTVGuide/src/jtvguide/jTVGuide.java
@@ -5,6 +5,8 @@
 
 package jtvguide;
 
+import xmltv.XMLTVParser;
+
 
 /**
  *
@@ -18,7 +20,7 @@ public class jTVGuide {
     public static void main(String[] args) {
     	System.out.println("jTVGuide version: $Rev$");
     	//XMLTVGrabber.grabSchedule();
-    	//XMLTVParser.parse();
+    	XMLTVParser.parse();
     }
 
 }
