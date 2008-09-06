9eb46d516ba9e1add779421cbb6c6aa4da11b8d1
michele.bologna <michele.bologna@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-09-06 16:31:53 +0000

    fix: problem if config/output filepath contains a space (e.g. C:\Documents ^ )

--- trunk/jTVGuide/src/it/unibg/cs/jtvguide/interfaces/xmltv/XMLTVConfigurator.java
+++ trunk/jTVGuide/src/it/unibg/cs/jtvguide/interfaces/xmltv/XMLTVConfigurator.java
@@ -3,7 +3,7 @@ package it.unibg.cs.jtvguide.interfaces.xmltv;
 import it.unibg.cs.jtvguide.UserPreferences;
 
 public interface XMLTVConfigurator {
-	public String CONFIG_SWITCH = "--configure --config-file " + UserPreferences.getXmltvConfigFile();
+	public String CONFIG_SWITCH = "--configure --config-file \"" + UserPreferences.getXmltvConfigFile() + "\"";
 	
 	public int configureXMLTV();
 }
