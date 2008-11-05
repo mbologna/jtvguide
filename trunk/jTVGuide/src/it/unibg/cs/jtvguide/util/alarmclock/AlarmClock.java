2e267fdb1a3329701adb78810d7a10287314aa94
michele.bologna <michele.bologna@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-11-05 12:12:55 +0000

    added alarmclock

---
+++ trunk/jTVGuide/src/it/unibg/cs/jtvguide/util/alarmclock/AlarmClock.java
@@ -0,0 +1,38 @@
+package it.unibg.cs.jtvguide.util.alarmclock;
+
+import it.unibg.cs.jtvguide.log.PublicLogger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AlarmClock {
+
+    private final Scheduler scheduler = new Scheduler();
+    private final SimpleDateFormat dateFormat =
+        new SimpleDateFormat("dd MMM yyyy HH:mm:ss.SSS");
+    private final int hourOfDay, minute, second;
+
+    public AlarmClock(int hourOfDay, int minute, int second) {
+        this.hourOfDay = hourOfDay;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void start() {
+        scheduler.schedule(new SchedulerTask() {
+            public void run() {
+                soundAlarm();
+            }
+            private void soundAlarm() {
+                PublicLogger.getLogger().info("Wake up! " +
+                    "It's " + dateFormat.format(new Date()));
+                // Start a new thread to sound an alarm...
+            }
+        }, new DailyIterator(hourOfDay, minute, second));
+    }
+
+    public static void main(String[] args) {
+        AlarmClock alarmClock = new AlarmClock(7, 0, 0);
+        alarmClock.start();
+    }
+}
