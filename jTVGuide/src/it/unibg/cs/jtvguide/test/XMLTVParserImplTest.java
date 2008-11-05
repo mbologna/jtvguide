821ecca92391579b91a16e14be24006089fcde5c
michele.bologna <michele.bologna@7d41ab44-7744-0410-b0b7-1f652f02dbac>
2008-11-05 16:38:17 +0000

    tests

---
+++ jTVGuide/src/it/unibg/cs/jtvguide/test/XMLTVParserImplTest.java
@@ -0,0 +1,30 @@
+package it.unibg.cs.jtvguide.test;
+
+import it.unibg.cs.jtvguide.xmltv.UserPreferences;
+import it.unibg.cs.jtvguide.xmltv.XMLTVParserImpl;
+import junit.framework.TestCase;
+
+public class XMLTVParserImplTest extends TestCase {
+XMLTVParserImpl xmltvpi;
+	
+	public static void main(String args[]) {
+		junit.textui.TestRunner.run(XMLTVScheduleInspectorTest.class);
+	}
+	
+	public void setUp() throws Exception {
+		UserPreferences.setXmltvOutputFile("examples/tv_grab.xml");
+		xmltvpi = new XMLTVParserImpl();
+	}
+
+	public void tearDown() throws Exception {
+		
+	}
+	
+	public void testParsing() {
+		assertEquals(true, xmltvpi.parse());
+		UserPreferences.setXmltvOutputFile("NON_EXISTS");
+		assertEquals(false,xmltvpi.parse());
+		UserPreferences.setXmltvOutputFile("examples/tv_grab-NON-VALID-XML.xml");
+		assertEquals(false,xmltvpi.parse());
+	}
+}
---
+++ jTVGuide/src/it/unibg/cs/jtvguide/test/XMLTVScheduleInspectorTest.java
@@ -0,0 +1,49 @@
+package it.unibg.cs.jtvguide.test;
+
+import it.unibg.cs.jtvguide.util.DateFormatter;
+import it.unibg.cs.jtvguide.xmltv.UserPreferences;
+import it.unibg.cs.jtvguide.xmltv.XMLTVScheduleInspector;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class XMLTVScheduleInspectorTest extends TestCase {
+	XMLTVScheduleInspector xmltvsi;
+	
+	public static void main(String args[]) {
+		junit.textui.TestRunner.run(XMLTVScheduleInspectorTest.class);
+	}
+	
+	public void setUp() throws Exception {
+		xmltvsi = new XMLTVScheduleInspector();
+		File f1 = new File("examples/tv_grab_update.xml");
+		FileWriter fw = new FileWriter(f1);
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 1);
+		fw.write("<programme start=\"" + DateFormatter.formatDate(new Date()));
+		fw.write("<programme start=\"" + DateFormatter.formatDate(c.getTime()));
+		fw.close();
+		UserPreferences.setXmltvOutputFile(f1.toString());	
+	}
+
+	public void tearDown() throws Exception {
+		xmltvsi = null;
+		UserPreferences.setXmltvOutputFile("tv_grab.xml");
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void testIsUpdate() {	
+		assertEquals(true, xmltvsi.isUpToDate());
+		assertEquals(false, xmltvsi.isUpToDate(new Date(111,9,19)));
+		UserPreferences.setXmltvOutputFile("NON_EXIST");
+		assertEquals(false, xmltvsi.isUpToDate());
+		assertEquals(false, xmltvsi.isUpToDate(new Date(111,9,19)));
+		UserPreferences.setXmltvOutputFile("tv_grab.xml");
+		assertEquals(false, xmltvsi.isUpToDate());
+		assertEquals(false, xmltvsi.isUpToDate(new Date(111,9,19)));
+	}
+}
